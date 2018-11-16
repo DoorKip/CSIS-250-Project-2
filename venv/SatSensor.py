a8b2bd5dac17d979a8f3b38fb74756cc1a070fc0
Robert Schult <robcschult@gmail.com>
2018-11-15 20:15:48 -0800

    Fixed improper json reference Added radio sat image

--- venv/SatSensor.py
+++ venv/SatSensor.py
@@ -7,6 +7,7 @@ Software Sensor Application
 
 import os
 import time
+import json
 import logging
 from datetime import datetime
 import requests
@@ -42,40 +43,44 @@ class SatSensor(SensorX):
                 print("Good Response")
                 content = self._create_content(response.text)
                 print("Content Created")
-                self._write_buffer(content)
+                if content is not None:
+                    self._write_buffer(content)
             else:
                 print("Bad Response")
-                return []
-
+                return None
         except (HTTPError, Timeout, ConnectionError, KeyError, ValueError, TypeError) as e:
             # logging.error("except: " + str(e))
             print("Error Occured:" + str(e))
             content = None
-
         return content
 
-    def _create_content(self, sat_json):
+    def _create_content(self, json_string):
         timestamp = datetime.now()
+        unixstamp = int(time.time())
         sats = []
+        sat_json = json.loads(json_string)
         print("Creating Content...")
-        if(len(sat_json)):
+        if(sat_json['info']['satcount'] > 0):
             for sat in sat_json["above"]:
                 sats.append(sat["satname"])
-
-        if(len(sats)):
-            d = {'k': timestamp,
-                 'date': timestamp.strftime('%Y-%m-%d %I:%M:%S %p'),
+            d = {'k': unixstamp,
                  'caption': 'Satelites over Grossmont',
-                 'summary': 'As of {} the following satelites are overhead: {}'.format(timestamp.strftime("%A %I:%M:%S %p"), ', '.join(map(str,sats)))
+                 'sum': 'Here are some of the sattelites currently passing over grossmont right now!',
+                 'story': 'As of {} the following satelites are overhead: {}'.format(timestamp.strftime("%B %d, %Y at %I:%M:%S %p"), ', '.join(map(str,sats))),
+                 'url img': self.get_featured_image()
                  }
             return [d]
+        else:
+            print("no sats")
+            return None
 
     def get_featured_image(self):
         sat_images = {
             "0": "https://s3.amazonaws.com/content.satimagingcorp.com/media/cms_page_media/1601/TH-01.jpg",
             "1": "https://spacenews.com/wp-content/uploads/2017/08/rsz_echostarJup3-879x485.jpg",
             "2": "https://upload.wikimedia.org/wikipedia/commons/0/04/International_Space_Station_after_undocking_of_STS-132.jpg",
-            "15": "http://www.davidiad.com/images/iridium/iridium_b.jpg"
+            "15": "http://www.davidiad.com/images/iridium/iridium_b.jpg",
+            "18": "https://s3.amazonaws.com/content.satimagingcorp.com/media/cms_page_media/1601/TH-01.jpg"
         }
         return sat_images[self.props.get('sat_category')]
 
@@ -83,14 +88,18 @@ class SatSensor(SensorX):
         """ return fresh or cached content"""
         if self._request_allowed():
             print("Requesting...")
-            return self._fetch_data()
+            data = self._fetch_data()
+            if data is not None:
+                return data
+            else:
+                return self._read_buffer()
         else:
             return self._read_buffer()
 
     def has_updates(self, k):
         """ find out if there is content beyond k """
         n = 0
-        content = self.get_all()  # newest last
+        content = self.get_all()
         for i in range(len(content)):
             if content[i]['k'] == k:
                 n = i + 1
@@ -98,7 +107,7 @@ class SatSensor(SensorX):
         return len(content) if n == 0 else len(content) - n
 
     def get_content(self, k):
-        content = self.get_all()  # newest last
+        content = self.get_all()
         n = 0
         for i in range(len(content)):
             if content[i]['k'] == k:
@@ -109,3 +118,8 @@ class SatSensor(SensorX):
 if __name__ == "__main__":
     sr = SatSensor()
     print("Sat Data : " + str(sr.get_all()))
+    time.sleep(20)
+    updates = sr.has_updates(0)
+    print("# Updates : " + str(updates))
+    if updates > 0:
+        print("Updates : " + str(sr.get_content(updates)))
